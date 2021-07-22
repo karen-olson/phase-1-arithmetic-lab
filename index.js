c2a85c160b0f868021c968624f23c4ee80bf97e8
Karen Olson <karmarie@gmail.com>
2021-07-22 17:03:29 -0500

    commit tree 0f9fb3c69e8113b0c58bc252f36f9f481d891448

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+//Multiply
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+//Random
+const random = Math.floor(Math.random() * 100) + 1;
+
+//Mod
+const num3 = 22;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+//Max
+
+const num5 = 1;
+const num6 = 2;
+const num7 = 20;
+
+const max = Math.max(num5, num6, num7);
+max;
